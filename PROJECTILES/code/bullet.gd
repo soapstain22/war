1e5ecfd431600915aba5ea8d42dacc9e36f4ff38
soapstain22 <36314018+soapstain22@users.noreply.github.com>
2024-06-27 20:59:02 -0400

    the test

--- PROJECTILES/code/bullet.gd
+++ PROJECTILES/code/bullet.gd
@@ -2,9 +2,6 @@ extends Node3D
 class_name Bullet
 @export var damage = 5
 @export var collisionShape:CollisionShape3D
-
-
-
 func _on_rigid_body_3d_body_entered(body):
 	if body.has_method("hit"):
 		body.hit(damage)
---
+++ code/destructable.gd
@@ -0,0 +1,8 @@
+extends RigidBody3D
+class_name Destructable
+@export var hitsound
+@export var scrapesound
+@export var contains
+@export var cra
+
+func attacked(dmg):
