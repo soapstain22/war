2b22489afc1209191f7eb2d208e2bd0d5f1d1ebb
Fouad Elamoury <foad.osama@hotmail.com>
2024-06-28 18:31:54 +0300

    Interacting with button with different keys

--- Interact/Interactable.gd
+++ Interact/Interactable.gd
@@ -5,6 +5,15 @@ class_name Interactable
 @export var prompt_input = "interact"
 
 
+func get_prompt():
+	var key_name = ""
+	for action in InputMap.action_get_events(prompt_input):
+		if action is InputEventKey:
+			key_name = action. as_text_physical_keycode ()
+			break
+	#print(prompt_message + "\n[" + key_name + "]")
+	return prompt_message + "\n[" + key_name + "]"
+	
 
 func interact(body):
-	print(body.name, "interacted with ", name)
+	print(body.name, " interacted with ", name)
--- code/destructable.gd
+++ code/destructable.gd
@@ -1,8 +1,9 @@
 extends RigidBody3D
-class_name Destructable
-@export var hitsound
-@export var scrapesound
-@export var contains
-@export var cra
+#class_name Destructable
+#@export var hitsound
+#@export var scrapesound
+#@export var contains
+#@export var cra
 
 func attacked(dmg):
+	print("attacked")
